fe32af799a7f04b60c38a3a2e523829cc0206105
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-13 17:30:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  const returnYear = record.find(superbowlWin)
+
+  function superbowlWin(record){
+
+    //   if (record.result === 'W'){
+    //       return record.year
+    //   }
+    
+    
+      for (let item of record){
+          if(item.result ==='W')
+          return item.year
+      }
+    
+
+  }
